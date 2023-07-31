95e99829ea424a20f7ca825a68ab38ed9ed9b7e7
Arcadio Arevalo <arevaloarcadio.96@gmail.com>
2023-07-31 15:44:53 -0400

    setImageAttribute ajuste en para login facebook y google

--- database/migrations/2021_02_12_000000_create_users_table.php
+++ database/migrations/2021_02_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('firstname');
             $table->string('lastname');
             $table->string('email',100)->unique();
-            $table->text('image');
+            $table->string('image',100);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->boolean('admin');
